4604d5e8b5c40f39dd77dfdbd21769f38f47de1f
David Liu <yliu3743@uwo.ca>
2022-12-19 05:18:33 -0500

    I correct some note and write other notes to the necessary parts.

--- R/GLMM.R
+++ R/GLMM.R
@@ -16,15 +16,15 @@
 #' the parameter should be a csv data set file
 #' GLMM function can create a GLMM model, that will return summary of statistics after analysis.
 
-# add the GLMM function
-GLMM <- function(data="ctsib.csv")
+# create the GLMM function
+GLMM <- function(data="ctsib.csv")# the argument is the data set
 {
-  ctsib = read.csv(data)
+  ctsib <- read.csv(data)# name the data
   ctsib <- ctsib %>%
     mutate(stable = 1 * (CTSIB == 1))
-  ctsib_fit <- run_model(ctsib, "ctsib")
-  n=1000
-  Bootstrap_result=matrix(nrow=n,ncol=5)
+  ctsib_fit <- run_model(ctsib, "ctsib")# fit the edited data
+  n = 1000
+  Bootstrap_result <- matrix(nrow=n,ncol=5)# use Bootstrap to fit the model
   for (i in 1:n){
     S = sample(1:480,size=480,replace=TRUE)
     X = ctsib[S,]
@@ -40,22 +40,24 @@ GLMM <- function(data="ctsib.csv")
   TS <- ctsib_fit$test_stat
   # create the p_value variable
   PR <- 2*pnorm(-abs(TS))
-  # sum the new variables to new tibble set
+  # fixed effect tibble set
   fixed.effect <- tibble(
     coef. = ctsib_fit$beta,
     std.err = SD[1:4],
-    # use coef and std.err to sum CI, the target result
+    # use coef and std.err to sum CI and other target result
     ci.lower = coef. - 1.96 * std.err,
     ci.upper = coef. + 1.96 * std.err,
     Z.value = TS,
     P.value = PR
   )
+  # random variance tibble set
   random.variance <- tibble(
     sigmasq = ctsib_fit$sigmasq,
     std.err = SD[5],
     ci.lower = sigmasq - 1.96 * std.err,
     ci.upper = sigmasq + 1.96 * std.err,
   )
+  # list the fixed effect, random variance, and random effect
   Return <- list(
     fixed.effect = fixed.effect,
     random.variance = random.variance,
