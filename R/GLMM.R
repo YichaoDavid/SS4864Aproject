fe72754ec675a3acb7ad5e0871a603657b6a7565
Cool-doggy-ZWX <115239592+Cool-doggy-ZWX@users.noreply.github.com>
2022-12-19 04:00:26 -0500

    update comment on the GLMM funtion

--- R/GLMM.R
+++ R/GLMM.R
@@ -11,6 +11,7 @@
 #' model <- GLMM("ctsib.csv")
 #' the parameter should be a csv dataset file
 #' GLMM function can create a GLMM model, that will return summary of statistics after analysis.
+# add the GLMM function
 GLMM <- function(data="ctsib.csv")
 {
   ctsib = read.csv(data)
@@ -26,10 +27,15 @@ GLMM <- function(data="ctsib.csv")
     Bootstrap_result[i,1:4] = X_fit$beta
     Bootstrap_result[i,5] = X_fit$sigmasq
   }
+  #transfer the result from the function to the tibble set
   Bootstrap_result <- as_tibble(Bootstrap_result)
+  # create standard deviation variable
   SD <- sapply(Bootstrap_result,sd)
+  #create test statistics variable
   TS <- ctsib_fit$test_stat
+  # create the p_value variable
   PR <- 2*pnorm(-abs(TS))
+  # sum the new variables to new tibble set 
   fixed.effect <- tibble(
     coef. = ctsib_fit$beta,
     std.err = SD[1:4],
