261149b275962f756ad4cc7dfac06bedc2fc1cfc
David Liu <yliu3743@uwo.ca>
2022-12-19 04:30:53 -0500

    change a little to the description

--- R/GLMM.R
+++ R/GLMM.R
@@ -1,16 +1,25 @@
-#' GLMM package with CTSIB.csv
+#' Generalized linear mixed effects model function package with data set CTSIB.csv
 #'
-#' @param data the dataset for analysis
+#' @param data the data set for analysis
 #'
-#' @return A list with summary statistics from the fitted model
+#' @return A list with summary statistics from the fitted model including fixed effect, random variance, and random effect.
 #' @import tidyverse
 #' @import lme4
 #' @export GLMM
 #'
+#' @usage GLMM(data)
+#' @author Yichao Liu, Weixuan Zhang
+#' @references
+#' Leppik, I.E., et al. (1985) A double-blind crossover evaluation of progabide in partial seizures.Neurology 35(285).
+#' McKeon CS, Stier AC, McIlroy SE, Bolker BM. Multiple defender effects: synergistic coral defense by mutualist crustaceans. Oecologia. 2012 Aug;169(4):1095-103.
+#' Ozgul et al. (2009) Upper respiratory tract disease, force of infection, and effects on survival of gopher tortoises. Ecological Applications 19(3):786-798.
+#' Steele, R. (1998). Effect of Surface and Vision on Balance. Ph.D. Thesis, Department of Physiotherapy, University of Queensland.
+#'
 #' @examples
 #' model <- GLMM("ctsib.csv")
-#' the parameter should be a csv dataset file
+#' the parameter should be a csv data set file
 #' GLMM function can create a GLMM model, that will return summary of statistics after analysis.
+
 # add the GLMM function
 GLMM <- function(data="ctsib.csv")
 {
@@ -35,7 +44,7 @@ GLMM <- function(data="ctsib.csv")
   TS <- ctsib_fit$test_stat
   # create the p_value variable
   PR <- 2*pnorm(-abs(TS))
-  # sum the new variables to new tibble set 
+  # sum the new variables to new tibble set
   fixed.effect <- tibble(
     coef. = ctsib_fit$beta,
     std.err = SD[1:4],
